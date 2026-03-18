cd0c46d6359c1d14d62ce7bb59c9d73f9c42b918
peter nadel <peter.m.nadel@gmail.com>
2026-03-18 13:25:06 -0400

    fixed index issues

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ if len(newspapers) > 0:
     event = st.plotly_chart(fig, on_select="rerun", selection_mode="points")
 
     if len(event['selection']['points']) > 0:
-        selected_year = math.floor(event['selection']['points'][0]["x"])
+        selected_year = math.floor(event['selection']['points'][0]["x"]) + 1
         amount = event['selection']['points'][0]["y"]
         selected_year = pd.to_datetime(f"1/1/{selected_year}")
         st.write(f"**Year selected**: {selected_year.strftime('%Y')}")
